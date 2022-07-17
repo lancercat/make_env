0e8427cbe4525bb65bbad25e1e07cee8434b9576
lasercat <lasercat@gmx.us>
2022-07-17 12:27:03 +0800

    meow

--- 01-pacman.sh
+++ 01-pacman.sh
@@ -4,3 +4,5 @@ sudo pacman -Syy
 sudo  pacman -S --needed $(comm -12 <(pacman -Slq | sort) <(sort paclist.txt))
 
 
+# drivers & kernels, you may want to change it.
+sudo pacman -S linux519 linux519-nvidia
