7dcc92a9003b9b30d2afc4f67168dfbdf735af98
lasercat <lasercat@gmx.us>
2022-07-17 12:29:25 +0800

    meow

--- 01-pacman.sh
+++ 01-pacman.sh
@@ -4,5 +4,8 @@ sudo pacman -Syy
 sudo  pacman -S --needed $(comm -12 <(pacman -Slq | sort) <(sort paclist.txt))
 
 
-# drivers & kernels, you may want to change it.
+# Manjaro-specific drivers & kernels, you may want to change it.
 sudo pacman -S linux519 linux519-nvidia
+
+# if you are on arch....
+# sudo pacman -S linux-nvidia
