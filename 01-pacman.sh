7816f4e5ee821c6d6f57c5f99eb3a6da39b66ed6
lancercat <59994105+lancercat@users.noreply.github.com>
2022-11-23 11:52:13 +0800

    Update 01-pacman.sh

--- 01-pacman.sh
+++ 01-pacman.sh
@@ -1,10 +1,4 @@
 sudo cp mirrorlist /etc/pacman.d/mirrorlist
 sudo pacman -Syy
 sudo  pacman -S --needed $(comm -12 <(pacman -Slq | sort) <(sort paclist.txt))
-
-
-# Manjaro-specific drivers & kernels, you may want to change it.
-sudo pacman -S linux519 linux519-nvidia
-
-# if you are on arch....
-# sudo pacman -S linux-nvidia
+sh kernels.sh
