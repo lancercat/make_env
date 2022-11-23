0b8f43595966f60387e7815217a70698d8cae71b
lasercat <lasercat@gmx.us>
2022-11-23 11:59:39 +0800

    tvupdate

--- torch_vision.sh
+++ torch_vision.sh
@@ -1,5 +1,8 @@
+TVVer=0.14.0
 echo "installing torch "
-unzip vision-0.13.0.zip -d build/
-cd build/vision-0.13.0
+rm v${TVVer}
+wget https://codeload.github.com/pytorch/vision/zip/refs/tags/v${TVVer} 
+unzip v${TVVer} -d build/
+cd build/vision-${TVVer}
 python setup.py install --user
 echo "done installing torch vison"
