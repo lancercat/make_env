6f8879044600dfa4e6de517dbe8446c04936bd9a
lasercat <lasercat@gmx.us>
2023-04-14 15:59:20 +0800

    linux62 torch2 and tv015.1

--- kernels.sh
+++ kernels.sh
@@ -1,6 +1,6 @@
 
 # Manjaro-specific drivers & kernels, you may want to change it.
-sudo pacman -S linux519 linux519-nvidia
+sudo pacman -S linux62 linux62-nvidia
 
 # if you are on arch....
 # sudo pacman -S linux-nvidia
--- torch_vision.sh
+++ torch_vision.sh
@@ -1,8 +1,13 @@
-TVVer=0.14.0
+TVVer=0.15.1
 echo "installing torch "
 rm v${TVVer}
+#rm build/ -r
 wget https://codeload.github.com/pytorch/vision/zip/refs/tags/v${TVVer} 
 unzip v${TVVer} -d build/
+
+#Fix ffmpeg: https://github.com/pytorch/vision/pull/7378/files#diff-769f780543a436bd40a765d14470d4eec50a7391367f0a44899dab8abefb2170
+cp stream.cpp build/vision-${TVVer}/torchvision/csrc/io/decoder/stream.cpp
+
 cd build/vision-${TVVer}
 python setup.py install --user
 echo "done installing torch vison"
