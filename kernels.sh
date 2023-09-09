da1ecc490c3c57b2c3a4af923df01050a4d3fe0a
lancercat <lasercat@gmx.us>
2023-09-09 09:54:53 +0800

    Fallback to linux61
    Manjaros have really short support period of non-lts kernels

--- kernels.sh
+++ kernels.sh
@@ -1,6 +1,6 @@
 
 # Manjaro-specific drivers & kernels, you may want to change it.
-sudo pacman -S linux64 linux64-nvidia
+sudo pacman -S linux61 linux61-nvidia
 
 # if you are on arch....
 # sudo pacman -S linux-nvidia
